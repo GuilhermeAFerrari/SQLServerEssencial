7ce1e7d4d1a776a5c1ded52c944fbede22af933f
guilhermeaferrari <guile.ferrari@hotmail.com>
2023-09-05 09:24:23 -0300

    feat: added file tips sql

---
+++ Tips.sql
@@ -0,0 +1,6 @@
+--To see statistics data, input and output time in the output messages
+
+SET STATISTICS TIME, IO ON
+
+--When using like operator, it it recommended to force the collation after the like statement
+COLLATE SQL_Latin1_General_CP1_CI_AI
